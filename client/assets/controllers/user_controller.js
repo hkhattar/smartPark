61a51dc746a1da2fe864468f89f75e9ec45421b1
hkhattar <himanshikhatter@gmail.com>
2017-08-02 12:04:12 -0700

    css

--- client/assets/controllers/user_controller.js
+++ client/assets/controllers/user_controller.js
@@ -1,11 +1,122 @@
 app.controller('user_controller', ['$scope','$cookies','$location','$anchorScroll','$routeParams','user_factory',
 
-    
-
  
     function($scope,$cookies,$location,$anchorScroll,$routeParams,user_factory)
     {
 
+
+
+
+
+
+
+
+
+// // var cities = [
+// //     {
+// //         city : 'Toronto',
+// //         desc : 'This is the best city in the world!',
+// //         lat : 43.7000,
+// //         long : -79.4000
+// //     },
+// //     {
+// //         city : 'New York',
+// //         desc : 'This city is aiiiiite!',
+// //         lat : 40.6700,
+// //         long : -73.9400
+// //     },
+// //     {
+// //         city : 'Chicago',
+// //         desc : 'This is the second best city in the world!',
+// //         lat : 41.8819,
+// //         long : -87.6278
+// //     },
+// //     {
+// //         city : 'Los Angeles',
+// //         desc : 'This city is live!',
+// //         lat : 34.0500,
+// //         long : -118.2500
+// //     },
+// //     {
+// //         city : 'Las Vegas',
+// //         desc : 'Sin City...\'nuff said!',
+// //         lat : 36.0800,
+// //         long : -115.1522
+// //     }
+// // ];
+
+
+// var latlng = new google.maps.LatLng(39.305, -76.617);
+// // map = new google.maps.Map(document.getElementById('map'), {
+// //   center: latlng,
+// //   zoom: 12
+// // });
+// //Angular App Module and Controller
+
+
+//     // var mapOptions = {
+//     //     zoom: 4,
+//     //     center: latlng,
+//     //     mapTypeId: google.maps.MapTypeId.TERRAIN
+//     // }
+
+//     $scope.map = new google.maps.Map(document.getElementById('map'), {
+//   center: latlng,
+//   zoom: 12
+// });
+
+//     $scope.markers = [];
+    
+//     var infoWindow = new google.maps.InfoWindow();
+    
+//     var createMarker = function (info){
+        
+//         var marker = new google.maps.Marker({
+//             map: $scope.map,
+//             position: new google.maps.LatLng(info.lat, info.long),
+//             title: info.city
+//         });
+//         marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+        
+//         google.maps.event.addListener(marker, 'click', function(){
+//             infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+//             infoWindow.open($scope.map, marker);
+//         });
+        
+//         $scope.markers.push(marker);
+        
+//     }  
+    
+//     for (i = 0; i < cities.length; i++){
+//         createMarker(cities[i]);
+//     }
+
+//     $scope.openInfoWindow = function(e, selectedMarker){
+//         e.preventDefault();
+//         google.maps.event.trigger(selectedMarker, 'click');
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
       // $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
       // $scope.username = "some name";
